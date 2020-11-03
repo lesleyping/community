ee82d1ea7818eeef3dc7aca770439e9fc7ba1498
lesleyping <741178981@qq.com>
2020-11-03 12:15:47 +0800

    帖子详情

--- src/main/java/com/lxp/community/controller/DiscussPostController.java
+++ src/main/java/com/lxp/community/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package com.lxp.community.controller;
 import com.lxp.community.entity.DiscussPost;
 import com.lxp.community.entity.User;
 import com.lxp.community.service.DiscussPostService;
+import com.lxp.community.service.UserService;
 import com.lxp.community.util.CommunityUtil;
 import com.lxp.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,6 +24,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -38,4 +44,20 @@ public class DiscussPostController {
         // 报错的情况将来统一处理
         return CommunityUtil.getJSONString(0,"发布成功！");
     }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        // 查询帖子
+        DiscussPost discussPost = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", discussPost);
+
+        // 帖子上要展示用户信息
+        // 第一种方式 用mybatis进行关联查询 业务耦合 因此不采用
+        // 第二种方式 通过帖子的userid，再次获得user，先采用这种
+        // 后期 会改为redis
+        User user = userService.findUserById(discussPost.getUserId());
+        model.addAttribute("user", user);
+        return "/site/discuss-detail";
+    }
+
 }
--- src/main/java/com/lxp/community/service/DiscussPostService.java
+++ src/main/java/com/lxp/community/service/DiscussPostService.java
@@ -37,4 +37,8 @@ public class DiscussPostService {
 
         return discussPostMapper.insertDiscussPost(post);
     }
+
+    public DiscussPost findDiscussPostById(int id) {
+        return discussPostMapper.selectDiscussPostById(id);
+    }
 }
