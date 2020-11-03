fe8633817d475a5930cb54ddbf758753f8e03230
lesleyping <741178981@qq.com>
2020-11-03 21:09:58 +0800

    添加评论

--- src/main/java/com/lxp/community/service/CommentService.java
+++ src/main/java/com/lxp/community/service/CommentService.java
@@ -2,15 +2,27 @@ package com.lxp.community.service;
 
 import com.lxp.community.dao.CommentMapper;
 import com.lxp.community.entity.Comment;
+import com.lxp.community.util.CommunityConstant;
+import com.lxp.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
 @Service
-public class CommentService {
+public class CommentService implements CommunityConstant {
     @Autowired
     private CommentMapper commentMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Autowired
+    private DiscussPostService discussPostService;
 
     public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
         return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
@@ -18,6 +30,26 @@ public class CommentService {
 
     public int findCommentsCount(int entityType, int entityId) {
         return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment) {
+        if (comment == null) {
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+        //——————添加评论————
+        // 过滤评论中的标签
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        // 过滤敏感词
+        comment.setContent(sensitiveFilter.filter(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        // ——————更新帖子评论数量————
+        if (comment.getEntityType() == ENTITY_TYPE_POST) {
+            int count = commentMapper.selectCountByEntity(comment.getEntityType(),comment.getEntityId());
+            discussPostService.updateCommentCount(comment.getEntityId(),count);
+        }
+        return rows;
     }
 
 }
--- src/main/java/com/lxp/community/service/DiscussPostService.java
+++ src/main/java/com/lxp/community/service/DiscussPostService.java
@@ -41,4 +41,8 @@ public class DiscussPostService {
     public DiscussPost findDiscussPostById(int id) {
         return discussPostMapper.selectDiscussPostById(id);
     }
+
+    public int updateCommentCount(int id, int commenCount) {
+        return discussPostMapper.updateCommentCount(id, commenCount);
+    }
 }
