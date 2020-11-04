4861c0fb6c0e60fbc05956f4a1b71acfb23ab4e0
lesleyping <741178981@qq.com>
2020-11-04 10:48:13 +0800

    私信列表

--- src/test/java/com/lxp/community/MapperTests.java
+++ src/test/java/com/lxp/community/MapperTests.java
@@ -2,9 +2,11 @@ package com.lxp.community;
 
 import com.lxp.community.dao.DiscussPostMapper;
 import com.lxp.community.dao.LoginTicketMapper;
+import com.lxp.community.dao.MessageMapper;
 import com.lxp.community.dao.UserMapper;
 import com.lxp.community.entity.DiscussPost;
 import com.lxp.community.entity.LoginTicket;
+import com.lxp.community.entity.Message;
 import com.lxp.community.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,8 @@ public class MapperTests {
     private DiscussPostMapper discussPostMapper;
     @Autowired
     private LoginTicketMapper loginTicketMapper;
+    @Autowired
+    private MessageMapper messageMapper;
     @Test
     public void testSelectUser(){
         User user = userMapper.selectById(101);
@@ -86,6 +90,29 @@ public class MapperTests {
         loginTicketMapper.updateStatus("abc", 1);
         loginTicket = loginTicketMapper.selectByTicket("abc");
         System.out.println(loginTicket);
+
+    }
+
+    @Test
+    public void testSelectLetters() {
+        List<Message> list = messageMapper.selectConversations(111, 0, 20);
+        for (Message message : list) {
+            System.out.println(message);
+        }
+
+        int count = messageMapper.selectConversationCount(111);
+        System.out.println(count);
+
+        list = messageMapper.selectLetters("111_112", 0, 10);
+        for (Message message : list) {
+            System.out.println(message);
+        }
+
+        count = messageMapper.selectLetterCount("111_112");
+        System.out.println(count);
+
+        count = messageMapper.selectLetterUnreadCount(131, "111_131");
+        System.out.println(count);
 
     }
 }
